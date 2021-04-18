b5b7f75e463bc45cdcd14afce0dc26003e408295
jacopo909 <60032905+jacopo909@users.noreply.github.com>
2021-04-18 17:28:06 +0200

    Add files via upload

---
+++ Progetto_Personale/Progetto_Personale/Aziendale.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Progetto_Personale
+{
+    /// <summary>
+    /// Logica di interazione per Window2.xaml
+    /// </summary>
+    public partial class Aziendale : Window
+    {
+        private string[] qualifiche = new string[] { "Dirigente", "Quadro", "Amministrativo", "Operaio" };
+        private PersonaleAziendale p;
+        public Aziendale(PersonaleAziendale p) // passo le variabili del personale aziendale tramite il costruttore, che serviranno per i parametri dichiarati nel form
+        {
+            this.p = p;
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
